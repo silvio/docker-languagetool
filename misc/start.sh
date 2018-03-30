67417bc9a8a35415ead3071b477ef5008ec49fdb
Silvio Fricke <silvio.fricke@gmail.com>
2018-03-30 10:06:23 +0200

    add ngrams support
    Please add a volume option to `docker run` command like `-v
    /path/to/ngrams:/ngrams`.
    
    This fixes #5.
    
    Signed-off-by: Silvio Fricke <silvio.fricke@gmail.com>

--- misc/start.sh
+++ misc/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*'
+EXTRAOPTIONS=""
+[ -d "/ngrams" ] && EXTRAOPTIONS=" --languageModel /ngrams "
+
+java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS}
