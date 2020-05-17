15a5d6c0f59e21047baf89ee7696da42817526da
Rahul Zhade <rzhade3@github.com>
2020-05-16 17:11:37 -0700

    a

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-$PATH_TO_FILE="$GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$2"
+PATH_TO_FILE="$GITHUB_WORKSPACE/$GITHUB_REPOSITORY/$2"
 cat $PATH_TO_FILE
 # java -jar /LanguageTool/languagetool-commandline.jar -l $1 $PATH_TO_FILE
