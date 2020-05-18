07032d2e905a6d6e2689f4a6b7ae65c6d21e8815
Rahul Zhade <rzhade3@github.com>
2020-05-18 00:10:59 -0700

    Ree

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -4,4 +4,4 @@ FILE_TO_PARSE=$2
 PATH_TO_FILE="$FILE_TO_PARSE"
 LOGFILE="$(date +%s).txt"
 java -jar /LanguageTool/languagetool-commandline.jar -l $1 --json $PATH_TO_FILE >> $LOGFILE
-python3 parse_log.py $FILE_TO_PARSE $LOGFILE
+python3 /parse_log.py $FILE_TO_PARSE $LOGFILE
