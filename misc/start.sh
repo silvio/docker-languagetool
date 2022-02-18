d7ebc6833cc578c4e8c8af2838c880ab899ddf31
Christian Segundo <36006540+someone-stole-my-name@users.noreply.github.com>
2022-02-18 18:36:54 +0100

    Fix typo

--- misc/start.sh
+++ misc/start.sh
@@ -3,4 +3,4 @@
 EXTRAOPTIONS=${EXTRAOPTIONS:-}
 [ -d "/ngrams" ] && EXTRAOPTIONS="${EXTRAOPTIONS} --languageModel /ngrams"
 
-java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS
+java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS}
