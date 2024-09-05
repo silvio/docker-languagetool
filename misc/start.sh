44cd0069c16e31b9db1b13a9f162e41a6a60ac4b
Andras Timar <andras.timar@collabora.com>
2024-09-05 10:13:29 +0200

    extend startup script
    - make EXTRAOPTION modifyable
    - add JAVAOPTIONS
    - add LT_ for https://languagetool.org/development/api/org/languagetool/server/HTTPServerConfig.html for e.g. enable PrometheusMetrics
    
    Contributed by @genofire at https://github.com/CollaboraOnline/online/pull/6929#issuecomment-1643631607
    
    Signed-off-by: Andras Timar <andras.timar@collabora.com>

--- misc/start.sh
+++ misc/start.sh
@@ -1,7 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-EXTRAOPTIONS=""
-[ -d "/ngrams" ] && EXTRAOPTIONS=" --languageModel /ngrams "
+EXTRAOPTIONS=${EXTRAOPTIONS:-}
+JAVAOPTIONS=${JAVAOPTIONS:-}
 
-java -cp languagetool-server.jar  org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS}
+if [ -d "/ngrams" ]; then
+    EXTRAOPTIONS="${EXTRAOPTIONS} --languageModel /ngrams"
+fi
+
+for var in ${!LT_*}; do
+  EXTRA_LT=true
+  echo "${var#'LT_'}="${!var} >> /tmp/config.properties
+done
+
+echo JAVAOPTIONS=$JAVAOPTIONS
+if [ "$EXTRA_LT" = true ]; then
+  EXTRAOPTIONS="${EXTRAOPTIONS} --config /tmp/config.properties"
+    echo config.properties:
+    echo "$(cat /tmp/config.properties)"
+fi
+echo EXTRAOPTIONS=$EXTRAOPTIONS
+
+java ${JAVAOPTIONS} -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' ${EXTRAOPTIONS}
